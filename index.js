6552fc98928a3acc8c1b893236d271de470fd852
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-04-27 17:14:16 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
-// Write your code here
+// Write your code her
+let num1 = 1, num2 = 62, multiply = num1*num2;
+function multiply(num1, num2){
+    return num1*num2; 
+};
+//=> 62
+let random = Math.floor(Math.random() * 100)
+//=>0 
+let num3 = 6, num4 = 28
+const mod = num4%num3; 
+//=> 4.666666
+let max = Math.max(2,4,6,8,10,12,14,16,18,20); 
